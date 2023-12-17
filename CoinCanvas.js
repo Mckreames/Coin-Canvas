abdd149a8015f9af8a48932312385959249f8b6e
Mckreames <114367512+Mckreames@users.noreply.github.com>
2023-12-17 09:01:01 -0500

    I spent the entire day trying out multiple API's and I'm not smart enough to make sense of any of them. Not even after watching  about 50 youtube videos and 5 tutorials

--- CoinCanvas.js
+++ CoinCanvas.js
@@ -2,55 +2,103 @@
 
 // PyGraph API AND Key From RapidAPI.com
 
-const url = "https://fygraph.p.rapidapi.com/graphql";
-const options = {
-  method: "POST",
-  headers: {
-    "x-rapidapi-key": "fa40ea65a6msha68e6f5343e548dp10595ajsn6d26736be691",
-    "x-rapidapi-host": "fygraph.p.rapidapi.com",
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    query: `query {getCryptoLatest {code marketCap name rank volume rate}}`,
-  }),
-};
+// const url = "https://fygraph.p.rapidapi.com/graphql";
+// const options = {
+//   method: "POST",
+//   headers: {
+//     "x-rapidapi-key": "fa40ea65a6msha68e6f5343e548dp10595ajsn6d26736be691",
+//     "x-rapidapi-host": "fygraph.p.rapidapi.com",
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     query: `query {
+//         getCryptoLatest {
+//             code
+//             marketCap
+//             name
+//             rank
+//             volume
+//             rate}}`,
+//   }),
+// };
 
-async function fetchData() {
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
+// async function fetchData() {
+//   try {
+//     const response = await fetch(url, options);
+//     const result = await response.json();
 
-    if (result.errors) {
-      console.error("GraphQL Errors:", result.errors);
-    } else {
-      displayData(result);
-    }
+//     if (result.errors) {
+//       console.error("GraphQL Errors:", result.errors);
+//     } else {
+//       //   displayData(result);
+//     }
 
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     console.log(result);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-function displayData(data) {
-  console.log(data);
-  const cryptoInfoSection = document.querySelector(".crypto-info");
-  const cryptoData = data.data.getCryptoLatest;
+// const ctx = document.getElementById("myChart");
 
-  const htmlContent = cryptoData
-    .map(
-      (item) => `
-            <div>
-                <p>Name: ${item.name}</p>
-                <p>Code: ${item.code}</p>
-                <p>Rank: ${item.rank}</p>
-                <p>Market Cap: ${item.marketCap}</p>
-            </div>
-            `
-    )
-    .join("");
+// new Chart(ctx, {
+//   type: "bar",
+//   data: {
+//     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//     datasets: [
+//       {
+//         label: "# of Votes",
+//         data: [12, 19, 3, 5, 2, 3],
+//         borderWidth: 1,
+//       },
+//     ],
+//   },
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   },
+// });
 
-  cryptoInfoSection.innerHTML = htmlContent;
-}
+// BitCoin Tools API
 
-fetchData();
+// (function (b, i, t, C, O, I, N) {
+//   window.addEventListener(
+//     "load",
+//     function () {
+//       if (b.getElementById(C)) return;
+//       (I = b.createElement(i)), (N = b.getElementsByTagName(i)[0]);
+//       I.src = t;
+//       I.id = C;
+//       N.parentNode.insertBefore(I, N);
+//     },
+//     false
+//   );
+// })(document, "script", "https://widgets.bitcoin.com/widget.js", "btcwdgt");
+
+// Widgets from CryptoCompare.com
+
+// function displayData(data) {
+//   console.log(data);
+//   const cryptoInfoSection = document.querySelector(".crypto-info");
+//   const cryptoData = data.data.getCryptoLatest;
+
+//   const htmlContent = cryptoData
+//     .map(
+//       (item) => `
+//             <div>
+//                 <p>Name: ${item.name}</p>
+//                 <p>Code: ${item.code}</p>
+//                 <p>Rank: ${item.rank}</p>
+//                 <p>Market Cap: ${item.marketCap}</p>
+//             </div>
+//             `
+//     )
+//     .join("");
+
+//   cryptoInfoSection.innerHTML = htmlContent;
+// }
+
+// fetchData();
