f8fbe31692cff376b6b125e9c5f8b3e64e1220b4
Mckreames <114367512+Mckreames@users.noreply.github.com>
2023-12-18 11:10:50 -0500

    sections are formatted correectly

--- CoinCanvas.js
+++ CoinCanvas.js
@@ -1,6 +1,81 @@
 `use strict`;
 
-// PyGraph API AND Key From RapidAPI.com
+// PyGraph API AND Key From RapidAPI.com via GraphQL
+const url = "https://fygraph.p.rapidapi.com/graphql";
+const options = {
+  method: "POST",
+  headers: {
+    "x-rapidapi-key": "fa40ea65a6msha68e6f5343e548dp10595ajsn6d26736be691",
+    "x-rapidapi-host": "fygraph.p.rapidapi.com",
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    query: `query MyQuery {
+        getCryptoLatest(code: "", limit: 10) {
+            date 
+            code 
+            delta {
+                day 
+                hour 
+                month 
+                quarter 
+                week 
+                year
+            } 
+            id 
+            marketCap 
+            name 
+            rank 
+            volume 
+            rate
+        }
+    }`,
+  }),
+};
+
+async function fetchData() {
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+
+    // Assign the result to the cryptoData variable
+    const cryptoData = result;
+
+    // Iterate through each crypto div
+    for (let i = 1; i <= 10; i++) {
+      const cryptoDiv = document.querySelector(`.crypto${i}`);
+      const cryptoInfo = cryptoData.data.getCryptoLatest[i - 1]; // Adjust the index
+
+      if (cryptoInfo) {
+        // Construct the content using the data
+        const content = `
+            <p>Date: ${cryptoInfo.date} |
+            Code: ${cryptoInfo.code} |
+            Rank: ${cryptoInfo.rank} |
+            Market Cap: ${cryptoInfo.marketCap} |
+            Volume: ${cryptoInfo.volume} |
+            Rate: ${cryptoInfo.rate} |
+            Delta (Day): ${cryptoInfo.delta.day}</p>
+            <!-- Add more fields as needed -->
+          `;
+
+        // Update the content of the crypto div
+        cryptoDiv.innerHTML = "";
+        cryptoDiv.innerHTML = content;
+      } else {
+        // Handle the case where there's no data for the div
+        cryptoDiv.innerHTML = "<p>No data available</p>";
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchData();
+// setInterval(fetchData, 1000);
 
 // const url = "https://fygraph.p.rapidapi.com/graphql";
 // const options = {
