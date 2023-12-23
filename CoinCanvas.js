3631da5defe01dd9ec22e8558574af68c147d951
Mckreames <114367512+Mckreames@users.noreply.github.com>
2023-12-22 23:43:31 -0500

    got the graph in the site. apparently "options" can't be used as a keyword. who would've thunk it

--- CoinCanvas.js
+++ CoinCanvas.js
@@ -1,12 +1,12 @@
 `use strict`;
 
 // Typing animation
-const type = new Typed(".auto-type", {
-  strings: ["Fortune", "Confidence", "Success", "Investments"],
-  typeSpeed: 80,
-  backSpeed: 40,
-  loop: true,
-});
+// const type = new Type(".auto-type", {
+//   strings: ["Fortune", "Confidence", "Success", "Investments"],
+//   typeSpeed: 80,
+//   backSpeed: 40,
+//   loop: true,
+// });
 
 // Modal Window
 const modal = document.querySelector(".modal");
@@ -32,8 +32,8 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
   //   modal.style.display = `block`;
 }
 
-btnClosedModal.addEventListener(`click`, closeModal);
-overlay.addEventListener(`click`, closeModal);
+// btnClosedModal.addEventListener(`click`, closeModal);
+// overlay.addEventListener(`click`, closeModal);
 
 document.addEventListener(`keydown`, function (e) {
   console.log(e.key);
@@ -45,7 +45,7 @@ document.addEventListener(`keydown`, function (e) {
 
 // PyGraph API AND Key From RapidAPI.com via GraphQL
 const url = "https://fygraph.p.rapidapi.com/graphql";
-const options = {
+const dataOptions = {
   method: "POST",
   headers: {
     "x-rapidapi-key": "fa40ea65a6msha68e6f5343e548dp10595ajsn6d26736be691",
@@ -79,7 +79,7 @@ const options = {
 
 async function fetchData() {
   try {
-    const response = await fetch(url, options);
+    const response = await fetch(url, dataOptions);
     const result = await response.json();
     console.log(result);
 
@@ -119,3 +119,26 @@ async function fetchData() {
 
 // fetchData();
 // setInterval(fetchData, 1000);
+
+baseUrl = "https://widgets.cryptocompare.com/";
+var bitcoin = document.querySelector(".bitcoin");
+var embedder = bitcoin;
+var cccTheme = {
+  Header: { background: "#d3e2f2", displayFollowers: false },
+  Followers: { color: "#f7931a" },
+  Data: { priceColor: "#f7931a" },
+  Chart: { animation: true, fillColor: "#ffc681", borderColor: "#f7931a" },
+  Trend: { colorUnchanged: "#f7931a" },
+};
+(function () {
+  var appName = encodeURIComponent(window.location.hostname);
+  if (appName == "") {
+    appName = "local";
+  }
+  var s = document.createElement("script");
+  s.type = "text/javascript";
+  s.async = true;
+  var theUrl = baseUrl + "serve/v1/coin/chart?fsym=BTC&tsym=USD";
+  s.src = theUrl + (theUrl.indexOf("?") >= 0 ? "&" : "?") + "app=" + appName;
+  embedder.parentNode.appendChild(s);
+})();
